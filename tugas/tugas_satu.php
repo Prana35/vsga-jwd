4f3741f822cb4bab6bd8531b65d24bc913268a7f
Prana35 <dafaaje222@gmail.com>
2024-05-21 17:47:13 +0700

    first commit

---
+++ tugas/tugas_satu.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nama: Prana Alfath Rais <br>"
+"Asal Kampus: Institut Informatika dan Bisnis Darmajaya <br>"
+"Jurusan: Teknik Informatika <br>"
+"Kota: Pringsewu <br>"
+"PHP: Pernah <br>"
+"HTML: Pernah <br>";
